99453d177fa64aaaaf531a420c9fc0286c405464
naviandres <naviandres88@gmail.com>
2020-01-19 00:16:17 -0500

    Primera version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
   ],
+  //modulos
   imports: [
     BrowserModule
   ],
+  //servicios inyectados
   providers: [],
+  //componente principal
   bootstrap: [AppComponent]
 })
 export class AppModule { }

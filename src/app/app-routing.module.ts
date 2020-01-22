cb1194fdfd0e84da88faf1c24b06f4b7b14acdde
naviandres <naviandres88@gmail.com>
2020-01-21 22:29:59 -0500

    routing movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule} from '@angular/router';
 
 import { PageNotFountComponent } from './components/page-not-fount/page-not-fount.component';
+import { MovieComponent } from './movie/movie.component';
+import { BodyComponent } from './components/body/body.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: '**' , component: PageNotFountComponent}
+  { path: '', redirectTo: 'BodyComponent', pathMatch: 'full'},
+  { path: 'BodyComponent', component:BodyComponent},
+  { path: 'MovieComponent', component:MovieComponent}
+  //{ path: '**' , component: PageNotFountComponent}
 ]
 
 

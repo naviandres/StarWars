d50246c506e6290fcf42971db38eda5fa80e1f51
Julian Malagon <jmalagon100@gmail.com>
2020-01-21 20:00:06 -0500

    internal_changes_CharCompoTS
    Cambios en Characters Components

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
   title = 'app.apiary.io Testing';
-  people: Observable<any>;
+  //people: Observable<any>;
 
   constructor(
-    private starWarsService: StarWarsService
+    //private starWarsService: StarWarsService
   ) { }
 
   ngOnInit() {
     //  this.people =  this.starWarsService.getHerosByCutom('https://swapi.co/api/people');
     // this.people = this.starWarsService.getViaAjax('https://swapi.co/api/people');
-     this.people = this.starWarsService.getHeros();;
+     //this.people = this.starWarsService.getHeros();;
   }
 
   postContent() {
@@ -20,4 +20,6 @@ export class AppComponent implements OnInit {
     // });
   }
 }
+
+export class CharactersComponent { }
 
